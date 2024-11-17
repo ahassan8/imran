2496bee65d1735aa4a303e4196fd3e382ff00136
ahassan8 <ahassan8844@gmail.com>
2024-11-17 00:20:05 -0500

    Update echeckout.js

--- mainjs/echeckout.js
+++ mainjs/echeckout.js
@@ -78,7 +78,7 @@ document.addEventListener("DOMContentLoaded", function() {
             }
 
             try {
-                const paymentIntentResponse = await fetch('https://imranfaith.com/create-payment-intent', {
+                const paymentIntentResponse = await fetch('https://api.imranfaith.com/create-payment-intent', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({ amount: ebookPriceInCents })
@@ -122,7 +122,7 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     // PayPal Button integration
-    fetch('https://imranfaith.com/get-paypal-client-id')
+    fetch('https://api.imranfaith.com/get-paypal-client-id')
         .then(response => response.json())
         .then(data => {
             const paypalClientId = data.clientId;
