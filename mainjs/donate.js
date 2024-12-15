8e6c97a8496d2dfa14a4d65fef4a2911f76b8699
ahassan8 <ahassan8844@gmail.com>
2024-12-14 19:50:56 -0500

    Update donate.js

--- mainjs/donate.js
+++ mainjs/donate.js
@@ -31,7 +31,7 @@ document.addEventListener("DOMContentLoaded", function () {
     let isProceedingToPayment = true; // State to track button behavior
 
     // Fetch Stripe and PayPal keys from the server
-    fetch('http://localhost:5000/get-stripe-publishable-key')
+    fetch('https://api.imranfaith.com/get-stripe-publishable-key')
         .then(response => response.json())
         .then(data => {
             stripePublicKey = data.publishableKey;
@@ -59,7 +59,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 }
 
                 try {
-                    const paymentIntentResponse = await fetch('http://localhost:5000/create-payment-intent', {
+                    const paymentIntentResponse = await fetch('https://imranfaith.com/create-payment-intent', {
                         method: 'POST',
                         headers: { 'Content-Type': 'application/json' },
                         body: JSON.stringify({ amount: donationAmountInCents }) // Send amount in cents
@@ -98,7 +98,7 @@ document.addEventListener("DOMContentLoaded", function () {
         });
 
     // Fetch PayPal Client ID and render PayPal button
-    fetch('http://localhost:5000/get-paypal-client-id')
+    fetch('https://api.imranfaith.com/get-paypal-client-id')
         .then(response => response.json())
         .then(data => {
             paypalClientId = data.clientId;
