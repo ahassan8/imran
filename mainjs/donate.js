ee5961d24c666885259aa42dae1ac6fbf35db9c8
ahassan8 <ahassan8844@gmail.com>
2024-12-28 20:55:09 -0500

    Update donate.js

--- mainjs/donate.js
+++ mainjs/donate.js
@@ -125,7 +125,7 @@ document.addEventListener("DOMContentLoaded", function () {
         const donationNote = document.getElementById("donation-note").value || "No note provided.";
 
         const emailPayload = {
-            to: "mysticvoiid2@gmail.com", // Replace with your admin email
+            to: "ahassan8844@gmail.com", // Replace with your admin email
             subject: "New Donation Received",
             html: `
                 <h1>New Donation</h1>
